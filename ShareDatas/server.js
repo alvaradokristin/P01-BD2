3bc3b3293e0aee583dd830b470b7b62803903fd9
Brenda Badilla Rodríguez <brendabbr2@hotmail.com>
2023-04-09 00:50:19 -0600

    Comments y general user

--- ShareDatas/server.js
+++ ShareDatas/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const multer = require("multer");
 const fs = require('fs');
 const app = express();
+const bodyParser = require('body-parser');
 
 const sqlCon = require("./sqldatabase");
 const moment = require('moment');
@@ -10,19 +11,36 @@ const uploadsql = multer({ dest: "sqluploads" });
 
 const path = require("path");
 const { render } = require("ejs");
-
+const mongoCon = require('./mongodb.js');
 app.set("view engine", "ejs");
 
 app.use(express.static(path.join(__dirname, "public")));
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+let mongo;
 
 // may not be necessary or it should be changed
 app.get("/", (req, res) => {
     res.render("index");
 })
 
-app.get("/dataset", (req, res) => {
-    res.render("./dataset");
+app.get("/dataset", async (req, res) => {
+    // const datasetid = "1"
+    const commentsCollection = mongo.collection('Comments');
+    const comments = await commentsCollection.find().toArray();
+    console.log(comments);
+    res.render("./dataset", { comments: comments });
 });
+
+app.post('/addComment/:dsid', (request, reponse) => {
+    const idds = request.params.dsid;
+    console.log(request.body);
+    const commentsCollection = mongo.collection('Comments');
+    commentsCollection.insertOne({datasetid: idds, userid: current_user, 
+    comment: request.body.comment, created_at: moment().format("DD/MM/YYYY - hh:MM")})
+}
+)
 
 app.get("/conversations", (req, res) => {
     const current_user = 'usuario1'
@@ -93,6 +111,10 @@ app.get("/user", (req, res) => {
     res.render("./user");
 });
 
+app.get("/generaluser", (req, res) => {
+    res.render("./generaluser");
+});
+
 app.get('/image/:id', (req, res) => {
     const id = req.params.id;
 
@@ -111,9 +133,11 @@ app.get('/image/:id', (req, res) => {
     );
 });
 
-app.listen(3000, function () {
+app.listen(3000, async function () {
     sqlCon.connect(function (err) {
         if (err) throw err;
         console.log("SQL Database Connected!")
     })
+    mongo = await mongoCon();
 });
+
